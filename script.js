b50c4a6ac583a96e1fbeb4f6b2bcf2975983735e
Jônathas Batista Silva <jonathasx57@gmail.com>
2022-03-01 18:36:15 -0300

    Ultimo commit

--- script.js
+++ script.js
@@ -3,9 +3,51 @@
 const resultado = document.getElementById("resultado");
 
 function clean() {
-  document.getElementById("resultado").innerHTML = "";
+    document.getElementById("resultado").innerHTML = "";
 }
 
 function insert(num) {
-  resultado.innerHTML += num;
+    resultado.innerHTML += num;
 }
+
+function calcular() {
+    document.getElementById("resultado").innerHTML = eval(resultado.innerHTML);
+}
+
+function diogoMoreira(sinal) {
+    document.querySelector("body").addEventListener("keydown", (tecla) => {
+        if (tecla.key == sinal) {
+            document.getElementById("resultado").innerHTML += sinal;
+        }
+    });
+}
+
+document.getElementById("limpar").addEventListener("click", () => {
+    clean();
+});
+
+document.querySelector("#calculo").addEventListener("click", () => {
+    calcular();
+});
+
+for (let i = 0; i <= 9; i++) {
+    diogoMoreira(i);
+}
+
+document.querySelector("body").addEventListener("keydown", (tecla) => {
+    if (tecla.key == "c") {
+        clean();
+    }
+});
+
+diogoMoreira(".");
+diogoMoreira("/");
+diogoMoreira("*");
+diogoMoreira("+");
+diogoMoreira("-");
+
+document.querySelector("body").addEventListener("keydown", (tecla) => {
+    if (tecla.key == "=") {
+        calcular();
+    }
+});
